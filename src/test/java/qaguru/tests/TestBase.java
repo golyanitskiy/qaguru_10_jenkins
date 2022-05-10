4cc9eba17ea85102ba3d8463b6c6878bcbca5c5f
Maksim Golyanitskiy <golyanitskiy@gmail.com>
2022-05-10 22:54:47 +0400

    edits

--- src/test/java/qaguru/tests/TestBase.java
+++ src/test/java/qaguru/tests/TestBase.java
@@ -1,22 +1,24 @@
 package qaguru.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import qaguru.helpers.Attach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
@@ -26,13 +28,14 @@ public class TestBase {
 
     @AfterAll
     static void closeDriver() {
-        Selenide.closeWebDriver();
+        closeWebDriver();
     }
 
-    @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
