bb4bde9ab2c5496414f3512e65074ae9336be596
Maksim Golyanitskiy <golyanitskiy@gmail.com>
2022-05-10 22:13:09 +0400

    doesn't work anyway

--- src/test/java/qaguru/tests/RegistrationFormPageTest.java
+++ src/test/java/qaguru/tests/RegistrationFormPageTest.java
@@ -1,5 +1,6 @@
 package qaguru.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import qaguru.pages.RegistrationFormPage;
 
@@ -7,6 +8,7 @@ import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormPageTest extends TestBase{
     @Test
+    @DisplayName("Successful form filling test")
     void formFilling() {
         RegistrationFormPage page = new RegistrationFormPage();
 
--- src/test/java/qaguru/tests/TestBase.java
+++ src/test/java/qaguru/tests/TestBase.java
@@ -2,6 +2,8 @@ package qaguru.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +13,7 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
