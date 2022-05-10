5a349eea23c1b1f2b12975c0559266575f4324bf
Maksim Golyanitskiy <golyanitskiy@gmail.com>
2022-05-10 23:23:43 +0400

    changed java version to 11 due to tests fail

--- src/test/java/qaguru/tests/TestBase.java
+++ src/test/java/qaguru/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import qaguru.helpers.Attach;
@@ -17,7 +18,7 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "2560x1600";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -31,6 +32,7 @@ public class TestBase {
         closeWebDriver();
     }
 
+    @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
